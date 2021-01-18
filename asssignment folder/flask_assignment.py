4702a06be0bd51b318e2226d916e8550ac0c4408
Coby Hong <cobyhong@gmail.com>
2021-01-18 14:37:27 -0800

    push

--- asssignment folder/flask_assignment.py
+++ asssignment folder/flask_assignment.py
@@ -1,7 +1,13 @@
 from flask import Flask
 from flask import request
 from flask import jsonify
+from flask_cors import CORS
+
+import random
+import string
+
 app = Flask(__name__)
+CORS(app) # <--- add this line
 
 users = { 
    'users_list' :
@@ -54,14 +60,27 @@ def hello_world():
 
 
 #http://127.0.0.1:5000/users/zap555
-@app.route('/users/<id>')
+@app.route('/users/<id>', methods=['GET', 'DELETE'])
 def get_user(id):
-   if id :
-      for user in users['users_list']:
-        if user['id'] == id:
-           return user
-      return ({})
-   return users
+   if request.method == 'GET':
+      if id :
+         for user in users['users_list']:
+            if user['id'] == id:
+               return user
+         return ({})
+      return users
+
+   elif request.method == 'DELETE':
+       if id :
+         count = 0
+         for user in users['users_list']:
+            if user['id'] == id:
+               users['users_list'].pop(count)
+               resp = jsonify(success=True)
+               resp.status_code = 201
+               return resp
+            count += 1
+         return users
 
 
 #POST / GETTER
@@ -90,20 +109,26 @@ def get_users():
 
    elif request.method == 'DELETE':
 
-       search_username = request.args.get('name')
-       if search_username :
+       search_id = request.args.get('id')
+       if search_id :
          count = 0
          for user in users['users_list']:
-            if user['name'] == search_username:
+            if user['id'] == search_id:
                users['users_list'].pop(count)
-               return users
+               resp = jsonify(success=True)
+               resp.status_code = 201
+               return resp
             count += 1
          return users
 
+   #add a code here I am guessing.
    elif request.method == 'POST':
       userToAdd = request.get_json()
+      userToAdd['id'] = random.choice(string.ascii_lowercase) + random.choice(string.ascii_lowercase) + random.choice(string.ascii_lowercase) + str(random.randint(0,9)) + str(random.randint(0,9)) + str(random.randint(0,9))
+      print(userToAdd)
       users['users_list'].append(userToAdd)
       resp = jsonify(success=True)
+      resp.status_code = 201
       #resp.status_code = 200 #optionally, you can always set a response code. 
       # 200 is the default code for a normal response
       return resp
